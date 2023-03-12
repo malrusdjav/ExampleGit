4e198b2604ad8fe8293388abcd98b5fd36267a68
rus.maliyev@mail.ru <rus.maliyev@mail.ru>
2023-03-12 13:19:36 +0300

    My first homework is done!

---
+++ src/com/test/Homework1.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class Homework1 {
+    public static void Main(String[] args){
+        task1();
+    }
+    public static void task1(){
+        System.out.println("There's the solution of my first homework");
+    }
+}
